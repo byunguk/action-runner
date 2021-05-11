5d51102267e3b79cb2a30d16f744ca11deff9890
byunguk <kbw815@gmail.com>
2021-05-11 14:51:18 -0700

    update scripts

--- emulator.sh
+++ emulator.sh
@@ -1,5 +1,5 @@
 ${ANDROID_HOME}/tools/bin/sdkmanager --update
-${ANDROID_HOME}/tools/bin/sdkmanager --install 'system-images;android-30;google_apis;x86_64;ndk;21.4.7075529'
+${ANDROID_HOME}/tools/bin/sdkmanager --install 'system-images;android-30;google_apis;x86_64'
 echo no | ${ANDROID_HOME}/tools/bin/avdmanager --verbose create avd --force -n 'TestAVD' -k 'system-images;android-30;google_apis;x86_64' -d pixel_c
 ${ANDROID_HOME}/tools/emulator -list-avds
 ${ANDROID_HOME}/tools/emulator @TestAVD -no-boot-anim -netdelay none -no-snapshot -wipe-data -no-audio -no-window
